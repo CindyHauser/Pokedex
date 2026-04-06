b9975087152dee5e9269049566f6f01b38803942
CindyHauser <cindy.hauser24@gmx.de>
2026-04-06 17:32:06 +0200

    -create a button to show the next 20 pokemon -push the names of all pokemon in an array to search for pokemon in the inputfield -styled the next-button styled the main-content

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-let pokemons = [];
+let pokemonNames = [];
 
-let BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
+let begin = 0;
 
 function init() {
-    fetchPokemon()
-}
+    fetchPokemon();
+    getPokemonNames();
+};
+
+//show pokemon and onclick-button for the next 20:
 
 async function fetchPokemon() {
+    let BASE_URL = `https://pokeapi.co/api/v2/pokemon?limit=0&offset=${begin}`;
     let response = await fetch(BASE_URL);
     let responseToJson = await response.json();
 
@@ -23,3 +27,28 @@ function renderPokemons(pokemonDetails) {
     let mainContentRef = document.getElementById('mainContent');
     mainContentRef.innerHTML += renderPokemonTemplates(pokemonDetails);
 };
+
+async function showNext20() {
+    begin += 20;
+    fetchPokemon();
+};
+
+//input-field to search for pokemon:
+
+async function getPokemonNames(){
+    let BASE_URL_NAMES = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    let responseName = await fetch(BASE_URL_NAMES);
+    let responseNameToJson = await responseName.json();
+
+    for (let indexName = 0; indexName < responseNameToJson.results.length; indexName++) {
+        let pokemonNamesRef = responseNameToJson.results[indexName];
+        pokemonNames.push(pokemonNamesRef.name);                 
+    };
+};
+
+function searchPokemon(){
+    let inputPokemonRef = document.getElementById('inputPokemon');
+    if (inputPokemonRef.value >= 3){
+        pokemonNames.filter()
+    }
+}
